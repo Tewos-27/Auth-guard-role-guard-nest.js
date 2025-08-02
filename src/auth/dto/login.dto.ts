45fb905877dd9e90bea69236f3e776a40304d199
Tewodros Shimels <tewodrosshimels54@gmail.com>
2025-08-02 10:37:01 +0100

    update all/changes

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,6 +1,8 @@
 import { IsEmail, IsString } from 'class-validator';
 
 export class LoginDto {
+  // This DTO is used for user login
+  
   @IsEmail({}, { message: 'Invalid email format' })
   email: string;
 
